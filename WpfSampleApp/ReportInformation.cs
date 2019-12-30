a81c95df02ca0c15f894584a8bd28a1d96d97e02
Rayffer <oscar.guillamon@gmail.com>
2019-12-30 16:12:29 +0100

    1. Mejoras SampleAPPWPF

--- WpfSampleApp/ReportInformation.cs
+++ WpfSampleApp/ReportInformation.cs
@@ -5,7 +5,7 @@ namespace WpfSampleApp
     public class ReportInformation
     {
         public string Name { get; set; }
-        public ReportType? Type { get; set; }
+        public ReportType Type { get; set; }
         public DateTime CreationDateTime { get; set; }
     }
 }
